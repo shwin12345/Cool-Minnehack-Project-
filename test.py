77d1041695a5578b62ef46dd605a201ee55f2bc7
ksidacroark <35880951+ksidacroark@users.noreply.github.com>
2018-01-27 23:51:17 -0600

    Add files via upload
    A NEEW GOD
    
    THIS IS GOD

--- test.py
+++ test.py
@@ -17,42 +17,94 @@ def getListen(recognizer):
 		return ":("
 	except sr.RequestError:
 		return ">:("
-   
+
+def fill(arr, num, val):
+	for i in range(num):
+		params.append(val)
+
+
+
 def stringMatch(string, list2):
 	for stre in list2:
 		if stre in string:
 			return True
-	return False 
+	return False
+
+
+   
+def stringMatchAll(string, list2):
+	DECISIONOFALIFETIMEPLEASEHELPME = False
+	for stre in list2:
+		if stre in string:
+			DECISIONOFALIFETIMEPLEASEHELPME = True
+		else:
+			DECISIONOFALIFETIMEPLEASEHELPME = False		
+
+	return DECISIONOFALIFETIMEPLEASEHELPME
+ 
+	
+		
+
+
 
 def yesNoVerify(recognizer):
 	result = getListen(recognizer)
 	if stringMatch(result, ["yes", "yeah", "definitely", "totally"]):
-		return 1
+		return True
 	elif stringMatch(result, ["no", "nah", "nope"]):
-		return 0
+		return False
 	else:
-		return 2
+		print("Invalid voice input, please try again!")
+		yesNoVerify(recognizer)
+	
 	
 
-def resultCheck(recognizer, keys, funcs, params):
+
+def resultCheckOR(recognizer, keys, funcs, params):
+	fill(params, len(funcs) - len(params), [])
 	result = getListen(recognizer)
 	for i, keylist in enumerate(keys):
 		for key in keylist:
 			if stringMatch(result, key):
-				funcs[i](*params[i])
-				return	
+				return funcs[i](*params[i])	
+	print("Invalid voice input, please retry.")
+	resultCheck(recognizer, keys, funcs, params)
+	
+
+def resultCheckAND(recognizer, keys, funcs, params):
+	fill(params, len(funcs) - len(params), [])
+	result = getListen(recognizer)
+	for i, keylist in enumerate(keys):
+		for key in keylist:
+			if stringMatchAll(result, key):
+				return funcs[i](*params[i])	
+	print("Invalid voice input, please retry.")
+	resultCheck(recognizer, keys, funcs, params)
+
+def resultCheckANDOR(recognizer, keyANDList, keyOR, funcs, ANDParamList, ORParamaList):
+	result = getListen(recognizer)
+	for i, keylist in enumerate(keyANDList):
+		for key in keylist:
+			if stringMatchAll(result, keylist):
+				return funcs[i](*ANDParamList[i])
+			elif stringMatch(result, keyOR[i]):
+				return funcs[i](*ORParamaList[i])
+	print("Invalid voice input, please retry.")
+	resultCheckANDOR(recognizer, keyANDList, keyOR, funcs, ANDParamList, ORParamaList)
+	
 
 
-
-
+	
 
 
 def noHeadache():
 	print("You have no headache? Great!")
+	return False
 
 
 def headache():
 	print("Oh! You have a headache :(")
+	return True
 
 
 
@@ -61,13 +113,18 @@ r = sr.Recognizer()
 
 
 print("Do you have a headache?")
-resultCheck(r, [
+boo = resultCheckOR(r, [
 	["yeah", "yes"],
 	["no", "nah", "nope"]
-], [headache, noHeadache], [
-[], 
-[]
-])
+], [headache, noHeadache], [])
+
+if boo:
+	print("Headache check!")
+
+
+
+
+
 
 
 
